2dda0a895a4ed6d020e95f4f999d3f9c2696d8f2
Doug Mason <dmason@unitedfiregroup.com>
2017-08-19 17:47:31 -0500

    Adding distance detector source.

---
+++ distance_detector.ino
@@ -0,0 +1,114 @@
+/*
+	Creation Crate Distance Detector
+ */
+
+// Step 1: Define inputs/outputs and key variables
+#define red2 13
+#define red1 12
+#define yellow2 11
+#define yellow1 10
+#define white2 9
+#define white1 8
+#define buzzer 3
+#define trigPin 7
+#define echoPin 6
+
+int sound = 250;
+long duration, distance;
+
+// Step 2: Create setup loop
+void setup() {
+	Serial.begin(9600);
+	
+	pinMode(trigPin, OUTPUT);
+	pinMode(echoPin, INPUT);
+	pinMode(white1, OUTPUT);
+	pinMode(white2, OUTPUT);
+	pinMode(yellow1, OUTPUT);
+	pinMode(yellow2, OUTPUT);
+	pinMode(red1, OUTPUT);
+	pinMode(red2, OUTPUT);
+	pinMode(buzzer, OUTPUT);
+}
+
+// Step 3: Create main loop
+void loop() {
+	/*
+	 Here, we are activating the Ultrasonic Sensor. The sensor sends out pulses of sound waves and
+	 uses them to measure the distance of an object (similar to a bat's echolocation).
+	 This 'low' pulse creates a delay so that the 'high' pulse gives a better signal.
+	 */
+	digitalWrite(trigPin, LOW);
+	delayMicroseconds(5);
+
+	// This 'high' pulse measures the distance of an object.
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin, LOW);
+
+	// 'pulseIn' calculates the amount of time it took for the pulse to return and stores it in 'duration'.
+	duration = pulseIn(echoPin, HIGH);
+
+	// The distance is calculated here in cm.
+	// To calculate in inches, replace the below line with this: distance = (duration/2) / 74;
+	distance = (duration / 2) / 29.1;
+
+	// This section of the code turns the LEDs on or off depending on the distance calculated.
+	if (distance <= 40) {
+		digitalWrite(white1, HIGH);
+		sound = 900;
+	}
+	else {
+		digitalWrite(white1, LOW);
+	}
+
+	if (distance < 30) {
+		digitalWrite(white2, HIGH);
+		sound = 1000;
+	}
+	else {
+		digitalWrite(white2, LOW);
+	}
+
+	if (distance < 20) {
+		digitalWrite(yellow1, HIGH);
+		sound = 1100;
+	}
+	else {
+		digitalWrite(yellow1, LOW);
+	}
+
+	if (distance < 15) {
+		digitalWrite(yellow2, HIGH);
+		sound = 1200;
+	}
+	else {
+		digitalWrite(yellow2, LOW);
+	}
+
+	if (distance < 10) {
+		digitalWrite(red1, HIGH);
+		sound = 1300;
+	}
+	else {
+		digitalWrite(red1, LOW);
+	}
+
+	if (distance < 5) {
+		digitalWrite(red2, HIGH);
+		sound = 1400;
+	}
+	else {
+		digitalWrite(red2, LOW);
+	}
+
+	// This turns the buzzer off if the object is too far.
+	if (distance > 40 || distance <= 0) {
+		noTone(buzzer);
+	}
+	else {
+		tone(buzzer, sound);
+	}
+
+	delay(50);
+}
